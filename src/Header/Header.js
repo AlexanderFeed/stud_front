974415ff92c950a51b34bfab22e6bb3164d045d5
AlexanderFeed <alex1mishchenko@gmail.com>
2023-08-23 18:05:24 +0300

    Небольшая разметка

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css';
+
+
+export default function Header() {
+  return (
+    <header class="header" >
+      <h1>Header</h1>
+    </header>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
+import Header from './Header/Header.js';
+import Sidebar from './Sidebar/Sidebar.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <Header/>
+    <Sidebar/>
   </React.StrictMode>
 );
